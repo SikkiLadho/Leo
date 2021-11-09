6bde4c7db58738a829519ea0dfaf2bd1b43d0d05
Sikki Ladho <mushi.shar@gmail.com>
2021-11-10 03:08:39 +0500

    preserved the device address set to x0 by rpi firmware

--- include/utils.h
+++ include/utils.h
@@ -4,5 +4,5 @@
 extern void delay ( unsigned long);
 extern void put32 ( unsigned long, unsigned int );
 extern unsigned int get32 ( unsigned long );
-extern void jump(void);
+extern void jump(unsigned long dt_address);
 #endif  /*_BOOT_H */
--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,10 @@
 #include "mini_uart.h"
 #include "utils.h"
-void kernel_main(void)
+void kernel_main(unsigned long dt_address)
 {
 	uart_init();
 	uart_send_string("Hello, world Hi!\r\n");
 
 
-	jump();
+	jump(dt_address);
 }
