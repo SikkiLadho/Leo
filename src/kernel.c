39c5f7cc7847c44780ed32e88f6f1e0e168fcbe6
SikkiLadho <mushi.shar@gmail.com>
2022-02-01 12:13:16 +0500

    refactor the code

--- src/kernel.c
+++ src/kernel.c
@@ -1,62 +1,63 @@
 #include "printf.h"
- #include "mini_uart.h"
- #include "utils.h"
-
-
+#include "mini_uart.h"
+#include "utils.h"
 
 int fdt_check_header(const void *fdt);
 int fdt_delprop(void *fdt, int nodeoffset, const char *name);
 int fdt_subnode_offset(const void *fdt, int parentoffset, const char *name);
-int fdt_first_property_offset(const void *fdt, int nodeoffset);
-const void *fdt_getprop_by_offset(const void *fdt, int offset,const char **namep, int *lenp);
 
-// register int _X0 __asm("x0");
-// register int _X1 __asm("x1");
-// register int _X2 __asm("x2");
-// register int _X3 __asm("x3");
-// register int _X4 __asm("x4");
-// register int _X5 __asm("x5");
-// register int _X6 __asm("x6");
-// register int _X7 __asm("x7");
-// register int _X8 __asm("x8");
-// register int _X9 __asm("x9");
-// register int _X10 __asm("x10");
-// register int _X11 __asm("x11");
-// register int _X12 __asm("x12");
-
-int fdt_check_header(const void *fdt);
-
-void kernel_main(const void * dt_address)
+void kernel_main( void * dtb_addr)
 {
-	int chosen_node_offset;	
-	if(fdt_check_header(dt_address) == 0)
-	{
-
-		printf("DTB is a valid DTB. \n");
-		chosen_node_offset= fdt_subnode_offset(dt_address, 0, "chosen");
-		if(fdt_delprop(dt_address, chosen_node_offset, "linux,initrd-end") == 0 && fdt_delprop(dt_address, chosen_node_offset, "linux,initrd-start") == 0)
-		{
-			printf("Initramfs props removed. \n");
-		}
-		else{
-			printf("Initramfs props could not be not removed. \n");
-		}
-
-	}
-	else
-	{
-		printf("DTB is not a valid DTB. \n");
-	}
+	
 	uart_init();
-	//uart_send_string("Hello\r\n");
+
+	printf("Leo Hypervisor Running...\n");
+
 	int el = get_el();
 	printf("Exception level: %d \r\n", el);
+	printf("DTB Address: %x \r\n", dtb_addr);
 
-	//printf("X0: %x \r\n", _X0);
-	printf("DTB Address: %x \r\n", dt_address);
+	/*
+
+	Kernel is being loaded into memory by using the initramfs configuration in /boot/config.txt. 
+	Initramfs config is usually used to load initrd/initramfs file[1]. When firmware reads the 
+	initramsconfig, it believes that an initrd image is being loaded, therefore firmware adds 
+	start and end address of the loaded image in chosen node of DTB[2].
+	The Linux kerenl in Raspbian does not use initrd images by default. However, after kernel sees the initrd 
+	addresses in the DTB, it expects the initrd image. In order to solve this, we need to remove the 
+	initrd properties from DTB before loading the kernel. We use libfdt library to remove these properties.
+
+	[1] https://www.raspberrypi.com/documentation/computers/config_txt.html#initramfs
+	[2] https://elinux.org/Device_Tree_Usage#chosen_Node
 	
+	*/
 
-	jump(dt_address);
+	int chosen;
+
+	//sanity check for DTB.
+	if (fdt_check_header((const void*)dtb_addr) == 0) {
+
+		printf("DTB is a valid DTB. \n");
+
+		//getting the offset to chosen node
+		chosen = fdt_subnode_offset((const void*)dtb_addr, 0, "chosen"); 
+
+		//deleting the "linux,initrd-end" and  "linux,initrd-start"
+		if (fdt_delprop(dtb_addr, chosen, "linux,initrd-end") == 0 && fdt_delprop(dtb_addr, chosen, "linux,initrd-start") == 0) {
+			printf("Deleted \"linux,initrd-end\" and  \"linux,initrd-start\" from DTB.\n");
+			printf("Jumping into Linux kernel...\n");
+		}
+		else {
+			printf("Could not delete delete \"linux,initrd-end\" and \"linux,initrd-start\" from DTB.\n");
+			printf("Virtual Machine might not work properly.\n");
+		}
+	}
+	else {
+		printf("DTB is not a valid DTB. Virtual Machine might not work properly.\n");
+	}
+	
+	//jumping to linux kernel image at EL1 with DTB address in the X0 register.
+	jump(dtb_addr);
 }
 
 
