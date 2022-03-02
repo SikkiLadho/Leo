a3f85cffd52ab390928e443a77364c1097b300f9
OsamaMuhammadd <osamaa.mohd001@gmail.com>
2022-03-02 20:46:33 +0500

    local commits

--- include/utils.h
+++ include/utils.h
@@ -4,10 +4,6 @@
 extern void delay ( unsigned long);
 extern void put32 ( unsigned long, unsigned int );
 extern unsigned int get32 ( unsigned long );
-<<<<<<< HEAD
 extern void jump(const void * dt_address);
-=======
-extern void jump(const void *dt_address);
->>>>>>> 44866fb86ddfd4d30093d7948ba0a1d804ea5946
 extern int get_el ( void );
 #endif  /*_BOOT_H */
