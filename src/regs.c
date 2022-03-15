401b54c170df1a48152057339f6af64e459c44c4
OsamaMuhammadd <osamaa.mohd001@gmail.com>
2022-03-15 18:25:57 +0500

    saving regs

--- src/regs.c
+++ src/regs.c
@@ -1,6 +1,10 @@
+#include "regs.h"
+
 typedef unsigned long int __uint64_t;
 typedef __uint64_t uint64_t;
 typedef uint64_t uintreg_t;
+
+
 struct regs
 {
     /* data */
@@ -8,3 +12,16 @@ struct regs
     uintreg_t x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30;
 
 };
+void save_regs(){
+
+    struct regs sregs;
+    sregs.x0=read_reg(x0);
+    sregs.x1=read_reg(x1);
+    sregs.x2=read_reg(x2);
+    sregs.x3=read_reg(x3);
+    sregs.x4=read_reg(x4);
+    sregs.x5=read_reg(x5);
+    sregs.x6=read_reg(x6);
+
+
+}
