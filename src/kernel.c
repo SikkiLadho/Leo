a5d0e21af7273094af26db5d09dbd46973e0a084
OsamaMuhammadd <osamaa.mohd001@gmail.com>
2022-03-18 19:53:25 +0500

    adding carriage return

--- src/kernel.c
+++ src/kernel.c
@@ -14,8 +14,12 @@ void kernel_main( void * dtb_addr)
 	
 	uart_init();
 
-	printf("Leo Hypervisor Running...\n");
-
+	printf("Leo Hypervisor Running...\r\n");
+	uart_send_string("Osama\r\n");
+	uart_send_string("Osama\r\n");
+	uart_send_string("Osama\r\n");
+	uart_send_string("Osama\r\n");
+	uart_send_string("Osama\r\n");
 	int el = get_el();
 	printf("Exception level: %d \r\n", el);
 	printf("DTB Address: %x \r\n", dtb_addr);
@@ -40,14 +44,14 @@ void kernel_main( void * dtb_addr)
 	//sanity check for DTB.
 	if (fdt_check_header((const void*)dtb_addr) == 0) {
 
-		printf("DTB is a valid DTB. \n");
+		printf("DTB is a valid DTB. \r\n");
 
 		//getting the offset to chosen node
 		chosen = fdt_subnode_offset((const void*)dtb_addr, 0, "chosen"); 
 
 		//deleting the "linux,initrd-end" and  "linux,initrd-start"
 		if (fdt_delprop(dtb_addr, chosen, "linux,initrd-end") == 0 && fdt_delprop(dtb_addr, chosen, "linux,initrd-start") == 0) {
-			printf("Deleted \"linux,initrd-end\" and  \"linux,initrd-start\" from DTB.\n");
+			printf("Deleted \"linux,initrd-end\" and  \"linux,initrd-start\" from DTB.\r\n");
 			int cpus_node;
 			cpus_node = fdt_subnode_offset((const void*)dtb_addr, 0, "cpus");
 			if(cpus_node >= 0)
@@ -68,18 +72,18 @@ void kernel_main( void * dtb_addr)
 			}
 			else
 			{
-				printf("OOPS. Not Found.\n");
+				printf("OOPS. Not Found.\r\n");
 			}
-			printf("Jumping into Linux kernel...\n");
+			printf("Jumping into Linux kernel...\r\n");
 
 		}
 		else {
-			printf("Could not delete delete \"linux,initrd-end\" and \"linux,initrd-start\" from DTB.\n");
-			printf("Virtual Machine might not work properly.\n");
+			printf("Could not delete delete \"linux,initrd-end\" and \"linux,initrd-start\" from DTB.\r\n");
+			printf("Virtual Machine might not work properly.\r\n");
 		}
 	}
 	else {
-		printf("DTB is not a valid DTB. Virtual Machine might not work properly.\n");
+		printf("DTB is not a valid DTB. Virtual Machine might not work properly.\r\n");
 	}
 	
 	//jumping to linux kernel image at EL1 with DTB address in the X0 register.
@@ -97,14 +101,14 @@ void spin_tbl_to_psci(void * dtb_addr, int cpu)
 
 					if(changed==0 && deleted == 0)
 					{
-						printf("CHANGED THE ENABLE METHOD TO PSCI.\n");
+						printf("CHANGED THE ENABLE METHOD TO PSCI.\r\n");
 					}
 					else
 					{
-						printf("Could not change enable method from to psci.\n");
+						printf("Could not change enable method from to psci.\r\n");
 					}
 
 				}
 				else
-				printf("oops, not found.\n");
+				printf("oops, not found.\r\n");
 }
--- src/print_regs.c
+++ src/print_regs.c
@@ -3,11 +3,11 @@
 void print_registers(){
 
     
-    printf("x6 -> %d \n",read_reg(x6));
-    printf("x7 -> %d \n",read_reg(x7));
-    printf("x8 -> %d \n",read_reg(x8));
-    printf("x9 -> %d \n",read_reg(x9));
-    printf("x10 -> %d \n",read_reg(x10));
-    printf("x11 -> %d \n",read_reg(x11));
+    printf("x6 -> %d \r\n",read_reg(x6));
+    printf("x7 -> %d \r\n",read_reg(x7));
+    printf("x8 -> %d \r\n",read_reg(x8));
+    printf("x9 -> %d \r\n",read_reg(x9));
+    printf("x10 -> %d \r\n",read_reg(x10));
+    printf("x11 -> %d \r\n",read_reg(x11));
 
 }
